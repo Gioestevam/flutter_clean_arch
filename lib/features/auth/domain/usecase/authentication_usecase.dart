2709646dd49ee5c46ce84ec12d1188ec0fed83fb
Giovanni Estevam <giovanni.estevam@sanar.com>
2022-08-25 17:32:21 -0300

    Update: adjust return method login

--- lib/features/auth/domain/usecase/authentication_usecase.dart
+++ lib/features/auth/domain/usecase/authentication_usecase.dart
@@ -1,8 +1,11 @@
 import 'package:dartz/dartz.dart';
 
-import '../../../../core/exception/type_exception.dart';
+import '../../../../core/error/failures.dart';
+import '../../../../core/params/login_param.dart';
+
+import '../../data/models/access_token_model.dart';
 
 abstract class AuthenticationUseCase {
-  Future<Either<TypeException, String>> login({required String email,required  String password});
+  Future<Either<Failure, AccessTokenModel>> login(LoginParam param);
   Future<void> reminderPassword();
 }
