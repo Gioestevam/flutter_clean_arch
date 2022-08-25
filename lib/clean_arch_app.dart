9382bb8e3b7783427d9fe3e9e5f6f480b61addb9
Giovanni Estevam <giovanni.estevam@sanar.com>
2022-08-25 01:20:40 -0300

    New: Bootstrap Page App

---
+++ lib/clean_arch_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CleanArchApp extends StatefulWidget {
+  const CleanArchApp({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _StateCleanArchApp();
+}
+
+class _StateCleanArchApp extends State<CleanArchApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+    );
+  }
+}
