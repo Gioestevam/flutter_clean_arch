e2d4e0b71e8e09d21c7d4b8aa2d5c76de90d3f58
Giovanni Estevam <giovanni.estevam@sanar.com>
2022-08-25 17:32:05 -0300

    Update: Adjust return method login

--- lib/features/auth/domain/repositories/authentication_repository.dart
+++ lib/features/auth/domain/repositories/authentication_repository.dart
@@ -1,7 +1,11 @@
 import 'package:dartz/dartz.dart';
-import 'package:flutter_clean_arc/core/exception/type_exception.dart';
+
+import '../../../../core/error/failures.dart';
+import '../../../../core/params/login_param.dart';
+
+import '../../data/models/access_token_model.dart';
 
 abstract class AuthenticationRepository {
-  Future<Either<TypeException, String>> login({required String email,required  String password});
-  Future<Either<TypeException, String>> passwordReminder(String email);
+  Future<Either<Failure, AccessTokenModel>> login(LoginParam param);
+  Future<Either<Failure, String>> passwordReminder(String email);
 }
