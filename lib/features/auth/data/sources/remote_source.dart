3f642e8ba57b055a608b833feea863b7550e1a2a
Giovanni Estevam <giovanni.estevam@sanar.com>
2022-09-04 16:45:46 -0300

    Update: remove file remote source

--- lib/features/auth/data/sources/remote_source.dart
+++
@@ -1,7 +0,0 @@
-import '../../../../core/params/login_param.dart';
-
-import '../models/access_token_model.dart';
-
-abstract class RemoteSource {
-  Future<AccessTokenModel> auth(LoginParam param);
-}
