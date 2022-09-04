f08d22c1e8ea39cf6a592e17b5172b9b9d85796f
Giovanni Estevam <giovanni.estevam@sanar.com>
2022-09-04 16:42:41 -0300

    Update: Add ensureInitialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'clean_arch_app.dart';
 import 'core/dependency_injection/dependency_injection_conf.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
   configureDependencies();
+  await Firebase.initializeApp();
   runApp(const CleanArchApp());
 }
