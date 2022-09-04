1a87490f9a2aefdcb93cb04a111d5dc72333c602
Giovanni Estevam <giovanni.estevam@sanar.com>
2022-09-04 16:42:28 -0300

    Update: Add new routes

--- lib/clean_arch_app.dart
+++ lib/clean_arch_app.dart
@@ -1,4 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'core/utils/theme_app.dart';
+import 'core/services/navigator_service.dart';
+import 'core/dependency_injection/dependency_injection_conf.dart';
+
+import 'features/auth/presenter/views/login/login_view.dart';
+import 'features/auth/presenter/cubits/login/login_cubit.dart';
+import 'features/auth/domain/usecase/login/login_usecase.dart';
+import 'features/auth/domain/usecase/logout/logout_usecase.dart';
+
+import 'features/home/presenter/views/home_view.dart';
+import 'features/home/presenter/cubit/home_cubit.dart';
+
+import 'features/splash/presenter/views/splash_view.dart';
+import 'features/splash/presenter/cubit/splash_cubit.dart';
 
 class CleanArchApp extends StatefulWidget {
   const CleanArchApp({Key? key}) : super(key: key);
@@ -11,7 +27,26 @@ class _StateCleanArchApp extends State<CleanArchApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(),
+      debugShowCheckedModeBanner: false,
+      theme: themeApp,
+      initialRoute: '/',
+      navigatorKey: NavigatorService.navigationKey,
+      routes: {
+        '/': (_) => BlocProvider(
+          create: (BuildContext context) => SplashCubit(),
+          child: SplashView()
+        ),
+        '/home': (_) => BlocProvider(
+          create: (BuildContext context) => HomeCubit(
+            logoutUsecase: getIt.get<LogoutUsecase>()
+          ),
+          child: HomeView()
+        ),
+        '/login': (_) => BlocProvider(
+          create: (BuildContext context) => LoginCubit(getIt.get<LoginUsecase>()),
+          child: LoginView()
+        )
+      },
     );
   }
 }
